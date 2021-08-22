77948e1ee34584049f3df65d041aee23d3d0d0ec
bryce_walker <bryce@teamatlas.cloud>
2021-08-22 14:36:18 +0100

    jetstream, cashier stripe installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [
-      './resources/**/*.blade.php',
+    mode: 'jit',
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
 
-      './resources/**/*.js',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
 
-      './resources/**/*.vue',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
